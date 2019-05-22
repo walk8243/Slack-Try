37143059b983c32438e30219031bdc6dc7ca4ac8
walk8243 <walk8243@gmail.com>
2019-05-23 00:35:52 +0900

    slackに送信する処理を追加

---
+++ models/Request.js
@@ -0,0 +1,43 @@
+const request = require('request');
+
+class Request {
+  get(url, data) {
+    return new Promise((resolve, reject) => {
+      request.get({
+        url: url,
+        json: data,
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      }, (error, response, body) => {
+        if(error) {
+          return reject(error);
+        }
+        resolve({
+          statusCode: response['statusCode'],
+          body: body,
+        });
+      });
+    });
+  }
+
+  post(url, data) {
+    return new Promise((resolve, reject) => {
+      request.post({
+        url: url,
+        json: data,
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      }, (error, response, body) => {
+        if(error) {
+          return reject(error);
+        }
+        resolve({
+          statusCode: response['statusCode'],
+          body: body,
+        });
+      });
+    });
+  }
+}
--- routes/slack.js
+++ routes/slack.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const Request = require('../models/Request');
+
+const slackInfo = {
+  token: 'xxxx',
+  channel: 'xxxx',
+};
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.send('slack');
 });
-router.post('/', (req, res, next) => {
+router.post('/', async (req, res, next) => {
   console.log(req.body);
+  const slackEvent = req.body['event'];
+  if(!Object.hasOwnProperty(slackEvent, 'subtype') || slackEvent['subtype'] == 'bot_message') {
+    const request = new Request();
+    await request.get('https://slack.com/api/chat.postMessage', { token: slackInfo['token'], channel: slackInfo['channel'], text: slackEvent['text'] });
+  }
   res.send(req.body['challenge']);
 });
 
