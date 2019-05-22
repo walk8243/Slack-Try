29da45ae791a816f590ed77ac3212ce9845b53dc
walk8243 <walk8243@gmail.com>
2019-05-23 01:03:02 +0900

    コンソールの停止

--- routes/slack.js
+++ routes/slack.js
@@ -12,14 +12,11 @@ router.get('/', function(req, res, next) {
   res.send('slack');
 });
 router.post('/', async (req, res, next) => {
-  console.log(req.body);
   const slackEvent = req.body['event'];
-  if(!Object.hasOwnProperty(slackEvent, 'subtype') || slackEvent['subtype'] == 'bot_message') {
+  if(!Object.hasOwnProperty(slackEvent, 'subtype') || slackEvent['subtype'] !== 'bot_message') {
     const slackData = { channel: slackInfo['channel'], text: slackEvent['text'] };
-    console.log(slackData);
     const request = new Request();
     const result = await request.post('https://slack.com/api/chat.postMessage', slackData, { 'Content-Type': 'application/json', Authorization: 'Bearer '+slackInfo['token'] });
-    console.log(result);
   }
   res.send(req.body['challenge']);
 });
