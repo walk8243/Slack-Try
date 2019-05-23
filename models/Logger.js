52ef971482cefceb175545381ade9acdd21f5b8e
walk8243 <walk8243@gmail.com>
2019-05-24 00:02:22 +0900

    アクセスログのパスを修正

--- models/Logger.js
+++ models/Logger.js
@@ -3,7 +3,7 @@ const log4js = require('log4js');
 log4js.configure({
   appenders: {
     system: { type: 'console' },
-    access: { type: 'file', filename: 'access.log' },
+    access: { type: 'file', filename: 'log/access.log' },
   },
   categories: {
     default: { appenders: [ 'system' ], level: 'debug' },
